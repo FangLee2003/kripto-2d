1675ccc0ec436e69c7d0c58978751aeac9941828
Fang Lee <phanhung18082003@gmail.com>
2022-09-05 22:43:03 +0700

    Beta 1.2 - Fixed bugs

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 8580;
+const port = process.env.PORT || 8080;
 const morgan = require('morgan');
 const path = require('path');
 const route = require('./src/routes');
